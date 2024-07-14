5d445dc3ac6e71c1610c53d8636c4e419ead414a
DragonZuxx <vunlhe170236@fpt.edu.vn>
2024-07-14 12:55:38 +0700

    Vu

--- src/java/Dao/WishlistDao.java
+++ src/java/Dao/WishlistDao.java
@@ -16,21 +16,22 @@ import Dal.DBContext;
 public class WishlistDao extends DBContext {
     PreparedStatement stm;
     ResultSet rs;
-
-
-    // Thêm sản phẩm vào danh sách yêu thích
-    public Wishlist addWishlist(int userId, int bookId) {
-        String sql = "INSERT INTO Wishlist VALUES(?, ?)";
+    public boolean addWishlist( Wishlist w) {
         try {
+            String sql = "INSERT INTO Wishlist (UserID, BookID) VALUES(?, ?)";
             stm = connection.prepareStatement(sql);
-            stm.setInt(1, userId);
-            stm.setInt(2, bookId);
-            stm.executeUpdate();
+            stm.setInt(1, w.getUserID());
+            stm.setInt(2, w.getBookID());
+            int result = stm.executeUpdate();
+            if (result > 0) {
+                return true;
+            }
         } catch (Exception e) {
             System.out.println("addWishlist: " + e.getMessage());
         }
-        return null;
+        return false;
     }
+
     // Lấy danh sách sản phẩm yêu thích của người dùng theo id
     public Wishlist getWishlistByUserId(int id) {
         String sql = "SELECT * FROM Wishlist WHERE UserID = ?";
@@ -52,12 +53,34 @@ public class WishlistDao extends DBContext {
         return null;
     }
     
-
-
-    //Test method
-    public static void main(String[] args) {
-        WishlistDao dao = new WishlistDao();
-        dao.addWishlist(1, 1);
+    //Lấy ra danh sanh sách yêu thích của người dùng theo UserID
+    public Wishlist getWishlistByUserIdAndBookId(int userId, int bookId) {
+        String sql = "SELECT * FROM Wishlist WHERE UserID = ? AND BookID = ?";
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setInt(1, userId);
+            stm.setInt(2, bookId);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                Wishlist wishlist = new Wishlist();
+                wishlist.setUserID(rs.getInt("UserID"));
+                wishlist.setBookID(rs.getInt("BookID"));
+                wishlist.setCreatedAt(rs.getTimestamp("CreatedAt").toLocalDateTime());
+                wishlist.setUpdatedAt(rs.getTimestamp("UpdatedAt").toLocalDateTime());
+                return wishlist;
+            }
+        } catch (Exception e) {
+            System.out.println("getWishlistByUserIdAndBookId: " + e.getMessage());
+        }
+        return null;
     }
 
+    //Test addWishlist
+    public static void main(String[] args) {
+        WishlistDao dao = new WishlistDao();
+        Wishlist w = new Wishlist();
+        w.setUserID(9);
+        w.setBookID(1);
+        System.out.println(dao.addWishlist(w));
+    }
 }
